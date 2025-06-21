f69528d08f63a3b30edd5e033dc46c67ebc629f0
Dennis Dahlberg <dennisdahlberg02@outlook.com>
2025-06-21 21:10:12 +0200

    Added class libraries for services and the data access layer

--- BlackJackMVC/Program.cs
+++ BlackJackMVC/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using BlackJackMVC.Data;
+using DataAccessLayer.Data;
 
 namespace BlackJackMVC;
 
--- DataAccessLayer/Data/ApplicationDbContext.cs
+++ DataAccessLayer/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace BlackJackMVC.Data;
+namespace DataAccessLayer.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
